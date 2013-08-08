8cdb80893a265d9db59444b2ae8becd488546bb4
Theo Ireton <theo@coldshift.za.net>
2013-08-09 00:31:25 +0200

    Certainly not owned by mxit

--- Categories/NSNotificationCenter+NotaflictionMonitor.h
+++ Categories/NSNotificationCenter+NotaflictionMonitor.h
@@ -1,9 +1,8 @@
 //
 //  NSNotificationCenter+NotaflictionMonitor.h
-//  MX
 //
 //  Created by Theo Ireton on 2013/08/08.
-//  Copyright (c) 2013 Mxit. All rights reserved.
+//  Copyright (c) 2013 HackerShack. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
